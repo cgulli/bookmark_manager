7de76ed317b87e696591a61fbf9e15c0fc321e09
Malinna Leach <malinnab@yahoo.com>
2016-10-19 12:13:07 +0100

    we can now add links

--- bookmark_manager/app/app.rb
+++ bookmark_manager/app/app.rb
@@ -3,9 +3,18 @@ require_relative 'models/link'
 
 class Bookmark < Sinatra::Base
 
-get '/links' do
-  @links = Link.all
-  erb(:links)
+  get '/links' do
+    @links = Link.all
+    erb(:links)
+    end
+
+  post '/links' do
+    Link.create(url: params[:url], title: params[:title])
+    redirect '/links'
+  end
+
+  get '/links/new' do
+    erb :new_links
   end
 
     run! if app_file == $0
